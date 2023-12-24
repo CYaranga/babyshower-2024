dd4741fc12f3e9e42a85547769234c676bb74692
Chris.Yaranga <christian.yaranga@pucp.pe>
2023-12-24 12:55:36 -0500

    Auto stash before cherry pick of "Updates"

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/babyshower-2024/',
+  base: 'https://cyaranga.github.io/babyshower-2024',
 })
